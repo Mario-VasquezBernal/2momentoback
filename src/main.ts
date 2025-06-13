0cbdd22dcd1233bea773e140fd270317a664bf8a
mario <mxvasquez@sudamericano.edu.ec>
2025-06-12 23:24:39 -0500

    enable CORS for Vercel frontend

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000); // ✅ Render usará PORT
+
+  // Habilitar CORS
+  app.enableCors({
+    origin: ['https://2momentofront.vercel.app'], // ← reemplaza con tu dominio real en Vercel
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT || 3000); // Render usará PORT
 }
 bootstrap();
